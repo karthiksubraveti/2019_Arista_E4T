f800514226bc2da3778fedec90cd89898f828113
Gaurav Verma <gauravv@arista.com>
2019-05-10 20:35:15 -0700

    update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/sh +x
-git clone https://github.com/karthiksubraveti/2019_Arista_E4T.git
-cd 2019_Arista_E4T
 sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py 
 sudo python3 get-pip.py
 sudo python3 -m pip install jupyter
