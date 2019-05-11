3093f7f00d797006845e9576c981e4626c40b458
Gaurav Verma <gauravv@arista.com>
2019-05-10 18:42:47 -0700

    Remove preview from desktopcam.py

--- html/desktopcam.py
+++ html/desktopcam.py
@@ -4,8 +4,5 @@ import time
 def capture(filename):
     camera = PiCamera()
     camera.resolution = (1024, 768)
-    camera.start_preview()
-    # sleep for the camera to warmup
-    time.sleep(1)
     camera.capture(filename)
     return True
