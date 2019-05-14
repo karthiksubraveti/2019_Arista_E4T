d9ab849f70ce50e58ce1734e0a519b1857af4c59
Gaurav Verma <gauravv@arista.com>
2019-05-14 15:00:57 -0700

    Add desktop cam code and update README

--- html/desktopcam.py
+++ html/desktopcam.py
@@ -1,6 +1,10 @@
-from picamera import PiCamera
 import time
+'''
+   Enable if you have connected raspberry pie camera module
 
+'''
+'''
+from picamera import PiCamera
 def capture(filename):
     camera = PiCamera()
     camera.resolution = (1024, 768)
@@ -10,3 +14,20 @@ def capture(filename):
     camera.stop_preview()
     camera.close()
     return True
+'''
+'''
+    Disable the following code if you have raspberry pi camera module
+    attached
+'''
+import cv2
+def capture(filename):
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+       return false
+    cap.set(3, 320)
+    cap.set(4, 240)
+    ret, image = cap.read()
+    cv2.imwrite(filename, image)
+    cap.release()
+    del(cap)
+    return ret
